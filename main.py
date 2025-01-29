b71d7ca464ab7ac1823972ee94aeb0d20d3d94c8
jordanryanf <jordan.farnham1031@gmail.com>
2025-01-29 10:09:29 -0500

    Astroidfield is a go!

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
+    
+    update_group = pygame.sprite.Group()
+    draw_group = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (update_group,draw_group)
+    Asteroid.containers = (asteroids, update_group, draw_group)
+    AsteroidField.containers = (update_group)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -10,12 +19,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    field = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.draw(screen)
+        for sprite in update_group:
+            sprite.update(dt)
+        for sprite in draw_group:
+            sprite.draw(screen)
+
         pygame.display.flip() 
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
